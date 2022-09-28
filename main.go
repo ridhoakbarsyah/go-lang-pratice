c0181009d1fa6f3ae4c13c755aa62c4b7cf835bb
ridhoakbarsyah <ridhoakbarsyah15@gmail.com>
2022-09-28 20:03:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nama string = "Ridho Akbarsyah"
+	var umur int = 22
+	var AsalKampus string = "Institut Teknologi Telkom Purwokerto"
+
+	fmt.Println("Nama saya adalah -->", nama)
+	fmt.Println("Umur saya adalah -->", umur)
+	fmt.Println("Asal Kampus saya -->", AsalKampus)
+}
